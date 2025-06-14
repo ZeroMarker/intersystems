f31e13f94f3b4f593c9eac37a3950e16cf748cda
Mark Chen <u78k4l67@163.com>
2025-06-14 19:14:48 +0800

    test

---
+++ easyui/index.js
@@ -0,0 +1,342 @@
+function test() {
+    let newDrugInfo = {
+        "infno": "3101",
+        "mdtrtarea_admvs": "430799",
+        "insuplc_admdvs": "430796",
+        "recer_sys_code": "IMS",
+        "dev_no": "",
+        "dev_safe_info": "",
+        "cainfo": "",
+        "signtype": "",
+        "infver": "V1.0",
+        "sign_no": "",
+        "opter_type": "1",
+        "opter": "医生01",
+        "opter_name": "医生01",
+        "inf_time": "2025-06-14 14:27:24",
+        "fixmedins_code": "CDSDERMYY",
+        "fixmedins_name": "常德市第二人民医院",
+        "mode": "1",
+        "input": {
+            "data": {
+                "rule_ids": [],
+                "trig_scen": "4",
+                "syscode": "IMS",
+                "patient_dtos": [
+                    {
+                        "patn_id": "1058158",
+                        "patn_name": "李弟秋",
+                        "gend": "1",
+                        "brdy": "1951-08-27",
+                        "poolarea": "430796",
+                        "curr_mdtrt_id": "2001668",
+                        "fsi_encounter_dtos": [
+                            {
+                                "mdtrt_id": "2001668",
+                                "medins_id": "CDSDERMYY",
+                                "medins_name": "常德市第二人民医院",
+                                "medins_admdvs": "430796",
+                                "medins_type": "A",
+                                "medins_lv": "01",
+                                "wardarea_codg": "100130",
+                                "wardarea_name": "十四病区",
+                                "wardno": "",
+                                "bedno": "10床",
+                                "setl_date": null,
+                                "adm_date": "2025-05-04 10:59:45",
+                                "dscg_date": null,
+                                "dscg_main_dise_codg": "XYZD018987",
+                                "dscg_main_dise_name": "痛风性关节炎",
+                                "dr_codg": "00129",
+                                "dr_name": "程勇",
+                                "prac_psn_codg": "00129",
+                                "prac_psn_name": "程勇",
+                                "adm_dept_codg": "骨科",
+                                "adm_dept_name": "100309",
+                                "dscg_dept_codg": "",
+                                "dscg_dept_name": "",
+                                "med_mdtrt_type": "1",
+                                "med_type": "21",
+                                "matn_stas": "0",
+                                "medfee_sumamt": 0,
+                                "ownpay_amt": 0,
+                                "selfpay_amt": 0,
+                                "acct_payamt": 0,
+                                "ma_amt": 0,
+                                "hifp_payamt": 0,
+                                "setl_totlnum": 1,
+                                "insutype": "390",
+                                "reim_flag": 0,
+                                "out_setl_flag": 0,
+                                "dise_no": "",
+                                "dise_name": "",
+                                "sinc_heal_lv": "",
+                                "his_md_cnt": "",
+                                "fsi_diagnose_dtos": [
+                                    {
+                                        "dise_id": 3069,
+                                        "inout_dise_type": "1",
+                                        "maindise_flag": 0,
+                                        "dias_srt_no": 1,
+                                        "dise_codg": "XYZD019082",
+                                        "dise_name": "膝关节退行性病变",
+                                        "dise_date": "2025-05-04 21:55:18"
+                                    },
+                                    {
+                                        "dise_id": 3071,
+                                        "inout_dise_type": "1",
+                                        "maindise_flag": 0,
+                                        "dias_srt_no": 2,
+                                        "dise_codg": "XYZD020255",
+                                        "dise_name": "腘窝囊肿",
+                                        "dise_date": "2025-05-04 21:55:18"
+                                    },
+                                    {
+                                        "dise_id": 7395,
+                                        "inout_dise_type": "1",
+                                        "maindise_flag": 1,
+                                        "dias_srt_no": 3,
+                                        "dise_codg": "XYZD018987",
+                                        "dise_name": "痛风性关节炎",
+                                        "dise_date": "2025-05-07 17:27:31"
+                                    },
+                                    {
+                                        "dise_id": 3070,
+                                        "inout_dise_type": "1",
+                                        "maindise_flag": 0,
+                                        "dias_srt_no": 4,
+                                        "dise_codg": "XYZD020123",
+                                        "dise_name": "膝关节滑膜炎",
+                                        "dise_date": "2025-05-04 21:55:18"
+                                    }
+                                ],
+                                "fsi_operation_dtos": [],
+                                "fsi_order_dtos": [
+                                    {
+                                        "rx_id": "6374",
+                                        "rxno": "IP202506140000022",
+                                        "grpno": "",
+                                        "long_drord_flag": 0,
+                                        "hilist_type": "101",
+                                        "chrg_type": "09",
+                                        "drord_bhvr": 0,
+                                        "hilist_code": "XG02BAZ061Z007010279543",
+                                        "hilist_name": "左炔诺孕酮宫内节育系统",
+                                        "hilist_lv": "--",
+                                        "hosplist_code": "XG02BAZ061Z007010279543",
+                                        "hosplist_name": "左炔诺孕酮宫内节育系统",
+                                        "hosplist_dosform": "栓剂",
+                                        "cnt": 1,
+                                        "pric": 1153,
+                                        "sumamt": 1153,
+                                        "selfpay_amt": 0,
+                                        "ownpay_amt": 0,
+                                        "spec": "52mg",
+                                        "spec_unt": "盒",
+                                        "drord_begn_date": "2025-06-14 14:25:52",
+                                        "drord_stop_date": null,
+                                        "drord_dept_codg": "100309",
+                                        "drord_dept_name": "骨科",
+                                        "drord_dr_codg": "ys01",
+                                        "drord_dr_name": "医生01",
+                                        "drord_dr_profttl": "231",
+                                        "curr_drord_flag": 1,
+                                        "order_id": 1,
+                                        "order_code": "XY1346",
+                                        "order_name": "左炔诺孕酮宫内节育系统",
+                                        "fee_type": "住院国家居民医保",
+                                        "self_paying_flag": null,
+                                        "order_state": "A",
+                                        "hosp_chrg_type": "09",
+                                        "hosp_chrg_type_name": "西药费",
+                                        "TarItemID": 6374
+                                    },
+                                    {
+                                        "rx_id": "6099",
+                                        "rxno": "IP202505220000142",
+                                        "grpno": "2",
+                                        "long_drord_flag": 1,
+                                        "hilist_type": "101",
+                                        "chrg_type": "09",
+                                        "drord_bhvr": 0,
+                                        "hilist_code": "XA11DAW036A001010104127",
+                                        "hilist_name": "维生素B1片",
+                                        "hilist_lv": "2",
+                                        "hosplist_code": "XA11DAW036A001010104127",
+                                        "hosplist_name": "维生素B1片",
+                                        "hosplist_dosform": "片剂",
+                                        "cnt": 1,
+                                        "pric": 0.1,
+                                        "sumamt": 0.1,
+                                        "selfpay_amt": 0,
+                                        "ownpay_amt": 0,
+                                        "spec": "10mg*100片/瓶",
+                                        "spec_unt": "瓶(100)",
+                                        "drord_begn_date": "2025-05-22 19:47:03",
+                                        "drord_stop_date": null,
+                                        "drord_dept_codg": "100309",
+                                        "drord_dept_name": "骨科",
+                                        "drord_dr_codg": "00508",
+                                        "drord_dr_name": "曾凡瑞",
+                                        "drord_dr_profttl": "232",
+                                        "curr_drord_flag": 0,
+                                        "order_id": 30119950,
+                                        "order_code": "XY1065",
+                                        "order_name": "维生素B1片",
+                                        "fee_type": "住院国家居民医保",
+                                        "self_paying_flag": 0,
+                                        "order_state": "A",
+                                        "hosp_chrg_type": "09",
+                                        "hosp_chrg_type_name": "西药费",
+                                        "TarItemID": 6099
+                                    },
+                                    {
+                                        "rx_id": "6109",
+                                        "rxno": "IP202505260000003",
+                                        "grpno": "1",
+                                        "long_drord_flag": 1,
+                                        "hilist_type": "101",
+                                        "chrg_type": "09",
+                                        "drord_bhvr": 0,
+                                        "hilist_code": "XA11GAW043A001010201815",
+                                        "hilist_name": "维生素C片",
+                                        "hilist_lv": "2",
+                                        "hosplist_code": "XA11GAW043A001010201815",
+                                        "hosplist_name": "维生素C片",
+                                        "hosplist_dosform": "片剂",
+                                        "cnt": 1,
+                                        "pric": 0.02,
+                                        "sumamt": 0.02,
+                                        "selfpay_amt": 0,
+                                        "ownpay_amt": 0,
+                                        "spec": "0.1g*100片/瓶",
+                                        "spec_unt": "瓶(100)",
+                                        "drord_begn_date": "2025-05-25 09:41:53",
+                                        "drord_stop_date": null,
+                                        "drord_dept_codg": "100309",
+                                        "drord_dept_name": "骨科",
+                                        "drord_dr_codg": "00508",
+                                        "drord_dr_name": "曾凡瑞",
+                                        "drord_dr_profttl": "232",
+                                        "curr_drord_flag": 0,
+                                        "order_id": 30122959,
+                                        "order_code": "XY1075",
+                                        "order_name": "维生素C片",
+                                        "fee_type": "住院国家居民医保",
+                                        "self_paying_flag": 0,
+                                        "order_state": "A",
+                                        "hosp_chrg_type": "09",
+                                        "hosp_chrg_type_name": "西药费",
+                                        "TarItemID": 6109
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+    let znsh_audit_timeout = 10000;
+    let auditHttp = 'http://114.242.246.230:11521';
+    $.support.cors = true;
+    $.ajax({
+        url: auditHttp + "/api/rest/hosAudit/ruleAudit",
+        type: "post", //GET
+        async: true, //true:异步，false:同步
+        data: JSON.stringify(newDrugInfo),
+        timeout: znsh_audit_timeout, //超时时间
+        contentType: "application/json;charset=utf-8",
+        dataType: 'json',
+        success: function (data, textStatus, jqXHR) {
+            var znsh_audit_no_dialog_3103_flag = false;
+            if (znsh_audit_no_dialog_3103 != null && znsh_audit_no_dialog_3103.length > 0) {
+                for (var i = 0; i < znsh_audit_no_dialog_3103.length; i++) {
+                    if (mode == znsh_audit_no_dialog_3103[i]) {
+                        znsh_audit_no_dialog_3103_flag = true;
+                        break;
+                    }
+                }
+            }
+            if (znsh_audit_no_dialog_3103_flag) {
+                // 自动反馈
+                var infcode = data["infcode"];
+                if (infcode == "0") {// 接口调用成功
+                    if (data.output != null && data.output.result != null && data.output.result.length > 0) {// 有违规信息
+                        var results = data.output.result;
+                        var jr_ids = [];
+                        for (var i = 0; i < results.length; i++) {
+                            var result = results[i];
+                            var jr_id = result["jr_id"];
+                            jr_ids.push(jr_id);
+                        }
+                        updateResultList(jr_ids, 1, "");
+                        var resData = { "code": 2 };
+                        callBack(resData);
+                    } else { // 接口调用成功，没有违规数据
+                        var resData = { "code": 0 };
+                        callBack(resData);
+                    }
+                } else {
+                    // 调用智能审核接口异常
+                    var resData = { "code": -1 };
+                    callBack(resData);
+                }
+            } else {
+                var flag = false;
+                if (znsh_audit_no_dialog != null && znsh_audit_no_dialog.length > 0) {
+                    for (var i = 0; i < znsh_audit_no_dialog.length; i++) {
+                        if (mode == znsh_audit_no_dialog[i]) {
+                            flag = true;
+                            break;
+                        }
+                    }
+                }
+                if (flag) {
+                    var resData = {
+                        "code": 0,
+                        "del": znsh_audit_dialog_delOrderId,
+                        "zifei": znsh_audit_dialog_selfPayingOrderId
+                    };
+                    callBack(resData);
+                } else {
+                    if (audit_sys_audit_mode_sz == 0) {
+                        if (znsh_audit_dialog_selfPayingCheck) {
+                            var param = {
+                                "medinsId": fixmedins_code,
+                                "isSelfPaying": 0
+                            };
+                            getRuleByCon(param, function (res) {
+                                znsh_audit_dialog_noSelfPaying = res
+                            });
+                        }
+                        // 单独调用院端智审
+                        handleData(data, callBack);
+                    } else if (audit_sys_audit_mode_sz == 1) {
+                        // 单独调用医保智审
+                        handleData_checkyb(data, callBack);
+                    } else if (audit_sys_audit_mode_sz == 2) {
+                        if (znsh_audit_dialog_selfPayingCheck) {
+                            var param = {
+                                "medinsId": fixmedins_code,
+                                "isSelfPaying": 0
+                            };
+                            getRuleByCon(param, function (res) {
+                                znsh_audit_dialog_noSelfPaying = res
+                            });
+                        }
+                        handleData_all(data, callBack);
+                    }
+                }
+            }
+        },
+        error: function (xhr, textStatus) {
+            var resData = {
+                "code": -1,
+                "del": znsh_audit_dialog_delOrderId,
+                "zifei": znsh_audit_dialog_selfPayingOrderId
+            };
+            callBack(resData);
+        }
+    });
+}
